a517db4563c608db8115c57a2e4e1694ef5595ac
Laurent Zubiaur <zubiaur.laurent@gmail.com>
2017-11-23 16:40:17 +0100

    Add comment about dependencies

--- src/Main.cpp
+++ src/Main.cpp
@@ -3,9 +3,14 @@
 #include "GL/glew.h"
 #include "GLFW/glfw3.h"
 
+// Depedencies:
+// GLEW http://glew.sourceforge.net/
+// GLFW http://www.glfw.org/download.html
+// ImGUI https://github.com/ocornut/imgui (no binaries available)
+
 // HACK: Include ImGUI cpp source files to avoid including then in the project.
 // Be sure to comment out the gl3w.h and glfw3.h includes in imgui_impl_glfw_gl3.cpp.
-// Copy glew32.dll and glfw3.dll in the output directory.
+// Don't forget to copy glew32.dll and glfw3.dll in the output directory.
 #include "imgui.h"
 #include "imgui.cpp"
 #include "imgui_draw.cpp"
